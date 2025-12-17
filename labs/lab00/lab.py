2dd15ffcf7fd6b0860cbb706147a4137359b828c
Sam Lau <samlau95@gmail.com>
2025-12-17 12:55:20 -0800

    lab 0

---
+++ labs/lab00/lab.py
@@ -0,0 +1,17 @@
+# lab.py
+
+
+from pathlib import Path
+import io
+import pandas as pd
+import numpy as np
+np.set_printoptions(legacy='1.21')
+
+
+# ---------------------------------------------------------------------
+# QUESTION 0
+# ---------------------------------------------------------------------
+
+
+def env_check():
+    ...
